e903ea7b1ba5d31dab6e0e387f8b3c89c7d95bcd
pdhondt <44711935+pdhondt@users.noreply.github.com>
2023-11-15 20:52:02 +0100

    refactord game logic

--- src/main/java/be/kunlabora/Game.java
+++ src/main/java/be/kunlabora/Game.java
@@ -13,18 +13,18 @@ public class Game {
                 return "You already placed a ship of this type. Choose another type.";
             }
         }
-        Ship aShip = new Ship(shipType, new Coordinate(x, y), orientation);
+        Ship aShip = new Ship(shipType, new Coordinate(x, y, Icon.SHIP), orientation);
         for (int i = 1; i < aShip.getShipType().getLength(); i++) {
             if (aShip.getOrientation().equals(Orientation.HORIZONTAL)) {
                 if (verifyPositionWithinLimits(y + i)) {
-                    aShip.getCoordinates().add(new Coordinate(x, y + i));
+                    aShip.getCoordinates().add(new Coordinate(x, y + i, Icon.SHIP));
                 } else {
                     return "Ship is too long and exceeds row limits";
                 }
             }
             if (aShip.getOrientation().equals(Orientation.VERTICAL)) {
                 if (verifyPositionWithinLimits(x + i)) {
-                    aShip.getCoordinates().add(new Coordinate(x + i, y));
+                    aShip.getCoordinates().add(new Coordinate(x + i, y, Icon.SHIP));
                 } else {
                     return "Ship is too long and exceeds column limits";
                 }
@@ -63,15 +63,29 @@ public class Game {
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= 10; j++) {
                 boolean isShipPresent = false;
+                boolean isDamaged = false;
+                boolean isSunk = false;
                 for (Ship ship : this.ships) {
                     for (Coordinate coordinate : ship.getCoordinates()) {
-                        if (i == coordinate.x() && j == coordinate.y()) {
+                        if (i == coordinate.getX() && j == coordinate.getY()) {
                             isShipPresent = true;
+                            if (coordinate.getStatus() == Icon.DAMAGE) {
+                                isDamaged = true;
+                            }
+                            if (coordinate.getStatus() == Icon.SINK) {
+                                isSunk = true;
+                            }
                             break;
                         }
                     }
                 }
-                if (isShipPresent) {
+                if (isShipPresent && isSunk) {
+                    output.append(Icon.SINK.getIcon());
+                }
+                else if (isShipPresent && isDamaged) {
+                    output.append(Icon.DAMAGE.getIcon());
+                }
+                else if (isShipPresent) {
                     output.append(Icon.SHIP.getIcon());
                 } else {
                     output.append(Icon.WAVE.getIcon());
@@ -85,12 +99,15 @@ public class Game {
     }
 
     public String fire(int x, int y) {
-        Coordinate firedAt = new Coordinate(x, y);
+        Coordinate firedAt = new Coordinate(x, y, Icon.SHIP);
         for (Ship ship : this.ships) {
             Set<Coordinate> coordinates = ship.getCoordinates();
             if (coordinates.contains(firedAt)) {
+                Coordinate hit = new Coordinate(x, y, Icon.DAMAGE);
                 coordinates.remove(firedAt);
-                if (coordinates.isEmpty()) {
+                coordinates.add(hit);
+                ship.setCoordinates(coordinates);
+                if (checkDestroyed(ship)) {
                     return "Ship Destroyed and Sinking!";
                 } else {
                     return "Hit!";
@@ -98,5 +115,23 @@ public class Game {
             }
         }
         return "Miss!";
+    }
+
+    public boolean checkDestroyed(Ship ship) {
+        int hitCount = 0;
+        Set<Coordinate> coordinates = ship.getCoordinates();
+        for (Coordinate coordinate : coordinates) {
+            if (coordinate.getStatus().equals(Icon.DAMAGE)) {
+                hitCount++;
+            }
+        }
+        if (hitCount == ship.getShipType().getLength()) {
+            for (Coordinate coordinate : coordinates) {
+                coordinate.setStatus(Icon.SINK);
+            }
+            ship.setCoordinates(coordinates);
+            return true;
+        }
+        return false;
     }
 }
