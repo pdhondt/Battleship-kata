d813ebdb13a3837f1f4dcb4b184a59f0a74a4354
pdhondt <44711935+pdhondt@users.noreply.github.com>
2023-11-15 20:49:43 +0100

    added status property and equals & hashcode methods to the Coordinate class

--- src/main/java/be/kunlabora/Coordinate.java
+++ src/main/java/be/kunlabora/Coordinate.java
@@ -1,4 +1,42 @@
 package be.kunlabora;
 
-public record Coordinate(int x, int y) {
+import java.util.Objects;
+
+public class Coordinate {
+    private final int x;
+    private final int y;
+    private Icon status;
+    public Coordinate(int x, int y, Icon status) {
+        this.x = x;
+        this.y = y;
+        this.status = status;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Icon getStatus() {
+        return status;
+    }
+
+    public void setStatus(Icon status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Coordinate that)) return false;
+        return getX() == that.getX() && getY() == that.getY() && getStatus() == that.getStatus();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getX(), getY(), getStatus());
+    }
 }
